09d2a19a2e47b2bcad09618bf154ae7f4db5d401
Nayana Davis <nayana487@gmail.com>
2016-08-31 09:11:36 -0400

    Update 03-spread-practice.js

--- 03-spread-practice.js
+++ 03-spread-practice.js
@@ -20,16 +20,4 @@ function copyReplace(array, from, to, elements) {
 
 console.log(copyReplace([1, 2, 100, 200, 6], 2, 4, [3, 4, 5]))
 
-// The third one is used to record a birdwatcher's sightings. It takes first a
-// timestamp (say, a Date), and then any number of strings naming birds. It
-// then stores these in the birdsSeen array.
-
-let birdsSeen = []
-function recordBirds(time) {
-    birdsSeen.push({time, birds: Array.prototype.slice.call(arguments, 1)})
-}
-
-recordBirds(new Date, "sparrow", "robin", "pterodactyl")
-console.log(birdsSeen)
-
 // source: http://marijnhaverbeke.nl/talks/es6_falsyvalues2015/exercises/#Spread_out
