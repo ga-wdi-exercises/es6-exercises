5395641e7b1720e9cdaed82e0b87287e90451897
Nicholas Olds <nolds9@yahoo.com>
2016-09-22 08:54:07 -0400

    Update 05-default-parameters.js

--- 05-default-parameters.js
+++ 05-default-parameters.js
@@ -1,16 +1,11 @@
-// It would be nice to be able to call our `lastIndexOf` function without
-// providing the third argument, and have it default to starting at the end of
-// the array. It would be even nicer if we could express this with an ES6
-// default argument value. Can we?
-
-// In which scope are default arguments evaluated? (Experiment to find out.)
-
-function lastIndexOf(arr, elt, start) {
-  for (let i = start - 1; i >= 0; i--)
-    if (arr[i] === elt) return i
-  return -1
-}
-
-console.log(lastIndexOf([1, 2, 1, 2], 2))
-
-// source: http://marijnhaverbeke.nl/talks/es6_falsyvalues2015/exercises/#Default_argument
+  /*
+   * Write a function that takes 3 parameters:
+   * greeting, firstName, and lastName
+   *
+   * Set the default values to 'hello', your first and last name,
+   * if no input is provided
+   */
+   
+   //const greeting =
+   // console.log(greeting());
+   // console.log(greeting('hi', 'harry', 'hedger'));
